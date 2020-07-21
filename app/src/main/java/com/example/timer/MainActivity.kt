e76664498b385a1d147f3afa440da7464b639cc8
hirokoshiroyama <inai.inai.baa777@gmail.com>
2020-07-21 21:34:57 +0900

    完成させる

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -16,15 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mTimer = Timer()
-        mTimer!!.schedule(object :TimerTask(){
-            override fun run() {
-                mTimerSec +=0.1
-                mHandler.post {
-                    timer.text = String.format("%.1f",mTimerSec)
-                }
-
+        start_button.setOnClickListener {
+            if (mTimer == null) {
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask() {
+                    override fun run() {
+                        mTimerSec += 0.1
+                        mHandler.post {
+                            timer.text = String.format("%.1f", mTimerSec)
+                        }
+                    }
+                }, 100, 100)
             }
-        },100,100)
+                pause_button.setOnClickListener {
+                if (mTimer != null) {
+                    mTimer!!.cancel()
+                    mTimer = null
+                }
+            }
+        }
+        reset_button.setOnClickListener{
+                mTimerSec = 0.0
+                timer.text = String.format("%.1f",mTimerSec)
+        }
+
     }
 }
